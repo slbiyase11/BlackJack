b36f1f9ab486fbf5194ccd2773f85a7e4e62abf0
SaziB <SaziB@ZAZN-UMH-95C9E0>
2021-12-16 16:47:00 +0200

    BlackJack

---
+++ BlackJack21-master/BlackJacke21/BlackJack21Game.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlackJacke21
+{
+    public class BlackJack21Game
+    {
+        public string PlayBlackJack(List<string> cardInput)
+        {
+            var result = string.Empty;
+
+            var getDealersCardsValue = GetDealerCardsInHandValue();
+
+            var playerCardValue = GetSumOfPlayersCardsInHand(cardInput);
+
+            var hasAce = DoesPlayersCardListHaveAces(cardInput);
+
+            if (cardInput.Count() >= 5 && playerCardValue < 21)
+            {
+                result = "Player Won";
+            }
+            else {
+
+                if (playerCardValue > 21 && hasAce)
+                {
+                    if (hasAce == true)
+                    {
+                        playerCardValue = (playerCardValue - 11) + 1;
+                    }
+
+                    if (playerCardValue >= getDealersCardsValue)
+                    {
+                        if (playerCardValue <= 21 || playerCardValue == getDealersCardsValue)
+                        {
+                            result = "Player Won";
+                        }
+                        else
+                        {
+                            result = "Dealer Won";
+                        }
+                    }
+                    else
+                    {
+                        result = "Dealer Won";
+                    }
+                }
+                else
+                {
+                    result = "Dealer Won";
+                }
+            }
+
+            return result;
+        }
+
+        private bool DoesPlayersCardListHaveAces(List<string> cardInput)
+        {
+            if (cardInput.Any(str => str.Contains("Ace")))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int GetDealerCardsInHandValue()
+        {
+            var cardInput = new List<string>() { "Jack of Spades", "Nine of Hearts" };
+            return GetSumOfPlayersCardsInHand(cardInput);
+        }
+
+        public int GetSumOfPlayersCardsInHand(List<string> cardInput)
+        {
+            var sum = 0;
+
+            foreach (var card in cardInput)
+            {
+                var cardValue = GetCardValue(card);
+                sum = sum + cardValue;
+            }
+
+            return sum;
+        }
+
+        
+
+        private int GetCardValue(string card)
+        {
+            var cardName = card.Substring(0, card.IndexOf(" ")).ToLower();
+            var cardValue = 0;
+
+            switch (cardName)
+            {
+                case "ace":
+                    cardValue = 11;
+                    break;
+
+                case "jack":
+                    cardValue = 10;
+                    break;
+
+                case "queen":
+                    cardValue = 10;
+                    break;
+
+                case "king":
+                    cardValue = 10;
+                    break;
+
+                case "nine":
+                    cardValue = 9;
+                    break;
+
+                case "eight":
+                    cardValue = 8;
+                    break;
+
+                case "seven":
+                    cardValue = 7;
+                    break;
+
+                case "six":
+                    cardValue = 6;
+                    break;
+
+                case "five":
+                    cardValue = 5;
+                    break;
+
+                case "four":
+                    cardValue = 4;
+                    break;
+
+                case "three":
+                    cardValue = 3;
+                    break;
+
+                case "two":
+                    cardValue = 2;
+                    break;
+
+                case "one":
+                    cardValue = 1;
+                    break;
+
+                default:
+                    cardValue = 0;
+                    break;
+            }
+
+            return cardValue;
+        }
+    }
+}
---
+++ BlackJack21-master/BlackJacke21Tests/BlackJack21GameTests.cs
@@ -0,0 +1,156 @@
+﻿using BlackJacke21;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace BlackJacke21Tests
+{
+    [TestFixture]
+    public class BlackJack21GameTests
+    {
+        [Test]
+        public void when_given_cards_return_sum()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Jack of Spades", "Nine of Hearts" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.GetSumOfPlayersCardsInHand(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(19));
+        }
+
+
+        [Test]
+        public void when_given_dealercards_return_sum_of_DealerCards_in_hand()
+        {
+            // Arrange
+            // Act
+            var sut = new BlackJack21Game();
+            int result = sut.GetDealerCardsInHandValue();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(19));
+        }
+
+        [Test]
+        public void when_player_places_seven_of_hearts_seven_of_diamonds_seven_of_clubs_six_of_spades_return_dealer_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Seven Of Spades", "Seven of Hearts", "Seven of Diamonds", "Six of Spades" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Dealer Won"));
+        }
+
+
+        [Test]
+        public void when_player_places_ace_of_hearts_seven_of_diamonds_seven_of_clubs_six_of_spades_return_player_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Ace Of Spades", "Seven of Hearts", "Seven of Diamonds", "Six of Spades" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Player Won"));
+        }
+
+        [Test]
+        public void when_player_places_ace_of_hearts_seven_of_diamonds_seven_of_clubs_five_of_spades_return_player_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Ace Of Spades", "Seven of Hearts", "Seven of Diamonds", "five of Spades" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Player Won"));
+        }
+
+
+        [Test]
+        public void when_player_places_an_ace_six_of_hearts_ace_of_diamonds_return_dealer_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Ace Of Spades", "Six of Hearts", "Ace of Diamonds"};
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Dealer Won"));
+        }
+
+        [Test]
+        public void when_player_places_an_ace_seven_of_hearts_ace_of_diamonds_return_player_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Ace Of Spades", "Seven of Hearts", "Ace of Diamonds" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Player Won"));
+        }
+
+
+        [Test]
+        public void when_player_andrew_places_diamonds_return_dealer_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "King Of Diamonds", "Four Of Spades", "Four of Clubs" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Dealer Won"));
+        }
+
+
+        [Test]
+        public void when_player_carl_places_queen_six_nine_diamonds_return_dealer_won()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Queen of Clubs", "Six Of Spades", "Nine of Diamonds" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Dealer Won"));
+        }
+
+
+        [Test]
+        public void when_player_bill_places_5_cards_and_less_than_twenty_one_player_wins()
+        {
+            // Arrange
+            var cardInput = new List<string>() { "Two of Spades", "Two Of Diamonds", "Two of Hearts", "Four of Diamonds", "Five of Clubs" };
+
+            // Act
+            var sut = new BlackJack21Game();
+            var result = sut.PlayBlackJack(cardInput);
+
+            // Assert
+            Assert.AreEqual("Player Won", result);
+        }
+
+    }
+}
